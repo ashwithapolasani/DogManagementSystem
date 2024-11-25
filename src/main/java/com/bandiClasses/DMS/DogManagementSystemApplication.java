417640d2e7b4eaa03d2be61a9f1315de44381bb2
ashwithapolasani <156382871+ashwithapolasani@users.noreply.github.com>
2024-11-24 20:42:04 -0600

    added dogmanagement classes and docker file

---
+++ src/main/java/com/bandiClasses/DMS/DogManagementSystemApplication.java
@@ -0,0 +1,18 @@
+package com.bandiClasses.DMS;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+ * @author Polasani Ashwitha Reddy (s566642)
+ * 
+ */
+
+@SpringBootApplication
+public class DogManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogManagementSystemApplication.class, args);
+	}
+
+}
